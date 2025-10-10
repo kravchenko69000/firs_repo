51b038c8a3c43bf8fd93df73087fba48211c9ec5
kravchenko69000 <kravchenko69000@gmail.com>
2025-10-10 10:33:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const loginBtn = document.getElementById("loginBtn");
 
-    loginBtn.addEventListener("click", () => {
-        const password = document.getElementById("password").value;
-        const correctPassword = "1234"; // заміни на свій пароль
-
-        if (password === correctPassword) {
-            document.getElementById("login").style.display = "none";
-            document.getElementById("content").style.display = "block";
-        } else {
-            alert("Невірний пароль!");
-        }
-    });
-});
